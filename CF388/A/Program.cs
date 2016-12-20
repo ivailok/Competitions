17c72a63c20d87b36d455bd7ebcf5c4d28451406
Ivaylo Kirilov <kirilov@cobuilder.no>
2016-12-20 10:48:14 +0200

    CF388

---
+++ CF388/A/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A
+{
+    class Program
+    {
+        static int N;
+
+        static void Main(string[] args)
+        {
+            N = int.Parse(Console.ReadLine());
+            if ((N & 1) != 0)
+            {
+                Console.WriteLine(((N - 3) >> 1) + 1);
+                for (int i = 0; i < ((N - 3) >> 1); i++)
+                {
+                    Console.Write("{0} ", 2);
+                }
+                Console.WriteLine(3);
+            }
+            else
+            {
+                Console.WriteLine(N >> 1);
+                for (int i = 0; i < (N >> 1); i++)
+                {
+                    if (i != 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(2);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
